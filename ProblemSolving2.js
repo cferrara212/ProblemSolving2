1e12df1c6689b803324572b710a12332574cfbe0
unknown <cferrara212@gmail.com>
2021-09-28 08:38:11 -0600

    commiting all current functions for happy numbers code.

--- ProblemSolving2.js
+++ ProblemSolving2.js
@@ -18,27 +18,47 @@
             //////
                         
                         let number = 19;
-                        let separatedNumber= separateNumbers(number);
-                        console.log(separatedNumber);
+                        let splitNumber= separateNumbers(number);
+
+                        // console logs to test functionality in different instances.
+                        console.log(splitNumber);
+                        console.log (separateNumbers(234));
+
                         
+            //////The number its self cannot be split, so we are going to turn the numebr into a string, split the string into pieces, putting them in an array, and then parse the array pieces back into numbers.
+            //////
+            ////// In this instance I will use the .map function instead of a for loop for simplicity and cleanliness. It will act the same as a for loop, iterating over the array and turning each portion of the
+            ////// of the array into a number and sending that number back to where the function was called. 
+            //////
+            ////// I will also be using the Number constructor in order to turn the string back into a number....thanks javascript, your so nice!
+
                         function separateNumbers(numberToSeparate)
                         {
-                            let numbersSeparated=[];
-                            for(let i=0; i<numberToSeparate.length; i++)
-                            {
-                                numbersSeparated.push(numberToSeparate[i]);
-                            }
-                            return numbersSeparated;
+                            return (numberToSeparate + '').split('').map((i) => {return Number(i); });
                         }
 
             //////
             ////// The second step is to find what the result of each number in that integer is when squared
             //////
+                        let splitNumbersSquared= squareNumbers(splitNumber);
 
+            //////  console logging function to ensure stability
+            /////
+                        console.log(squareNumbers(splitNumber));
+                        function squareNumbers(numbersToSquare)
+                        {
+                            return splitNumber.map(function(i) {return i * i});
+                        }
             //////
             ////// The third step is to add the sum of the squared numbers
             //////
-
+                       for (let i=0; i<splitNumbersSquared.length; i++)
+                       {
+                           let added;
+                           added= added + splitNumbersSquared[i];
+                           
+                       }
+                       console.log(added);
             //////
             ////// The fourth setp is to repeat this untill a single digit is reached
             //////
